61bb6cf310a09cbed60fef1f85cacf655c710551
Orlando Arzola <orlando.arzola@externos.bci.cl>
2019-02-07 23:15:08 -0300

    Initial Commit

---
+++ PaymentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by Nisum on 2/7/19.
+//  Copyright © 2019 Orlando Arzola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
